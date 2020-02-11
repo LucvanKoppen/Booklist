f6d8e2f96e654e9f4bbde7797edf781f7039868a
LucvanKoppen <1046921@mborijnland.nl>
2020-02-11 17:53:26 +0100

    final release

--- src/Controller/ReviewController.php
+++ src/Controller/ReviewController.php
@@ -31,7 +31,6 @@ class ReviewController extends AbstractController
     public function new(Request $request): Response
     {
         $review = new Review();
-//        $review = $review->setBookId($);
         $form = $this->createForm(ReviewType::class, $review);
         $form->handleRequest($request);
 
